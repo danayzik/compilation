a00fec2359680432e2b226be509127138efec8d5
danayzik <danayzik@mail.tau.ac.il>
2024-12-03 01:35:45 +0200

    ..

---
+++ ex2/src/AST/AST_DEC_ARRAY_TYPE_DEF.java
@@ -0,0 +1,6 @@
+package AST;
+//not finished
+public class AST_DEC_ARRAY_TYPE_DEF extends AST_DEC
+{
+
+}
---
+++ ex2/src/AST/AST_DEC_CLASS_DEC.java
@@ -0,0 +1,6 @@
+package AST;
+//not finished
+public class AST_DEC_CLASS_DEC extends AST_DEC
+{
+
+}
